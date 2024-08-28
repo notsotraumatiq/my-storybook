58f9ed6377ab8d28cb9738f20d1276acbed5bff5
notsotraumatiq <atiqpatel95@gmail.com>
2024-08-28 17:09:40 -0400

    more config

--- src/stories/ProfileCard/ProfileCard.stories.tsx
+++ src/stories/ProfileCard/ProfileCard.stories.tsx
@@ -1,9 +1,14 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from "@storybook/react";
 
-import ProfileCard from './ProfileCard';
+import ProfileCard from "./ProfileCard";
 
 const meta = {
   component: ProfileCard,
+  title: "Profile Card",
+  tags: ["autodocs"],
+  parameters: {
+    layout: "centered",
+  },
 } satisfies Meta<typeof ProfileCard>;
 
 export default meta;
